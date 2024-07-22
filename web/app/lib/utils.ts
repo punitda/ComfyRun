ff49e37e1bd35e14f422cf077d62ad28c5843193
punitda <punitdama@gmail.com>
2024-07-22 19:59:29 +0530

    Add useDebounce hook

--- web/app/lib/utils.ts
+++ web/app/lib/utils.ts
@@ -1,6 +1,35 @@
 import { type ClassValue, clsx } from "clsx";
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function useDebounce<T = unknown>(
+  value: T,
+  delay: number
+): [T, boolean] {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  const [isDebouncing, setDebouncing] = useState(false);
+  useEffect(
+    () => {
+      // Update debounced value after delay
+      setDebouncing(true);
+      const handler = setTimeout(() => {
+        setDebouncing(false);
+
+        setDebouncedValue(value);
+      }, delay);
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
+  return [debouncedValue, isDebouncing];
 }
