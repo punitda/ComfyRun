8044b58bee1c491ee8559cce59000091164f27b7
punitda <punitdama@gmail.com>
2024-07-24 19:24:25 +0530

    Format file

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
+from typing import Dict, List, Literal
 from pydantic import BaseModel, HttpUrl
-from typing import Dict, List, Literal, Optional
 
 from fastapi import FastAPI
 
@@ -13,23 +13,27 @@ class CustomNode(BaseModel):
     state: Literal["not-installed"]
     hash: str
 
+
 class CustomNodes(BaseModel):
     custom_nodes: Dict[HttpUrl, CustomNode]
     unknown_nodes: List[str]
-    
+
+
 class Model(BaseModel):
     name: str
     url: HttpUrl
     path: str
-    
+
+
 class Gpu(str, Enum):
-    ANY="Any"
-    T4="T4"
-    L4="L4"
-    A10G="A10G"
-    A100="A100"
-    H100="H100"
-    
+    ANY = "Any"
+    T4 = "T4"
+    L4 = "L4"
+    A10G = "A10G"
+    A100 = "A100"
+    H100 = "H100"
+
+
 class CreateMachinePayload(BaseModel):
     machine_name: str
     gpu: Gpu
@@ -39,8 +43,9 @@ class CreateMachinePayload(BaseModel):
 
 @app.post("/create-machine")
 def create_machine(payload: CreateMachinePayload):
-    return {"machine_name": payload.machine_name, 
-            "gpu" : payload.gpu.value, 
-             "custom_nodes": payload.custom_nodes,
-            "models": payload.models}
-    
+    return {
+        "machine_name": payload.machine_name,
+        "gpu": payload.gpu.value,
+        "custom_nodes": payload.custom_nodes,
+        "models": payload.models,
+    }
