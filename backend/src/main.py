d8c4686ab89a0ce88b4f3de899dcf19812c911b8
punitda <punitdama@gmail.com>
2024-07-25 16:06:18 +0530

    deploy machine yay!!

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,18 @@
 from enum import Enum
+import asyncio
+import os
+import json
 
 from typing import Dict, List, Literal
 from pydantic import BaseModel, HttpUrl
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 
+from dotenv import load_dotenv
+from slugify import slugify
 
+load_dotenv()
 app = FastAPI()
 
 
@@ -42,10 +49,48 @@ class CreateMachinePayload(BaseModel):
 
 
 @app.post("/create-machine")
-def create_machine(payload: CreateMachinePayload):
+async def create_machine(payload: CreateMachinePayload):
+    asyncio.create_task(deploy_machine(payload))
     return {
         "machine_name": payload.machine_name,
         "gpu": payload.gpu.value,
         "custom_nodes": payload.custom_nodes,
         "models": payload.models,
     }
+
+
+async def deploy_machine(payload: CreateMachinePayload):
+    folder_path = f"/app/builds/{payload.machine_name}"
+    cp_process = await asyncio.create_subprocess_exec("cp", "-r", "/app/src/template", folder_path)
+    await cp_process.wait()
+
+    config = {
+        "machine_name": slugify(payload.machine_name),
+        "gpu": payload.gpu
+    }
+
+    os.makedirs(os.path.dirname(f"{folder_path}/config.py"), exist_ok=True)
+    os.makedirs(os.path.dirname(f"{folder_path}/models.json"), exist_ok=True)
+    os.makedirs(os.path.dirname(
+        f"{folder_path}/custom_nodes.json"), exist_ok=True)
+
+    with open(f"{folder_path}/config.py", "w", encoding='utf-8') as f:
+        f.write("config = " + json.dumps(config))
+
+    with open(f"{folder_path}/custom_nodes.json", "w", encoding='utf-8') as f:
+        json.dump(jsonable_encoder(payload.custom_nodes), f, indent=4)
+
+    with open(f"{folder_path}/models.json", "w", encoding='utf-8') as f:
+        json.dump(jsonable_encoder(payload.models), f, indent=4)
+
+    process = await asyncio.create_subprocess_shell(
+        "modal deploy workflow.py",
+        cwd=folder_path,
+        env={**os.environ,
+             "MODAL_TOKEN_ID": os.getenv("MODAL_TOKEN_ID"),
+             "MODAL_TOKEN_SECRET": os.getenv("MODAL_TOKEN_SECRET"),
+             "COLUMNS": "10000",
+             }
+    )
+
+    await process.wait()
