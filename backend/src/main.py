f1d4ab970d32b6b55b34fe3a5a13da7364525b29
punitda <punitdama@gmail.com>
2024-07-26 11:19:42 +0530

    Create generate-custom-nodes Route
    Added script code from ComfyUI manager to generate list of used nodes from workflow file

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,15 +2,18 @@ from enum import Enum
 import asyncio
 import os
 import json
+import re
 
-from typing import Dict, List, Literal
+from typing import Dict, List, Literal, Annotated
 from pydantic import BaseModel, HttpUrl
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 from fastapi.encoders import jsonable_encoder
 
 from dotenv import load_dotenv
 from slugify import slugify
+
+from src.node_map import ext_node_map
 
 load_dotenv()
 app = FastAPI()
@@ -60,6 +63,13 @@ async def create_machine(payload: CreateMachinePayload):
     }
 
 
+@app.post("/generate-custom-nodes")
+async def generate_custom_nodes(workflow_file: Annotated[bytes, File()]):
+    workflow = json.loads(workflow_file.decode("utf-8"))
+    used_exts, _ = await extract_nodes_from_workflow(workflow)
+    return used_exts
+
+
 async def deploy_machine(payload: CreateMachinePayload):
     folder_path = f"/app/builds/{payload.machine_name}"
     cp_process = await asyncio.create_subprocess_exec("cp", "-r", "/app/src/template", folder_path)
@@ -95,3 +105,88 @@ async def deploy_machine(payload: CreateMachinePayload):
     )
 
     await process.wait()
+
+
+async def extract_nodes_from_workflow(workflow):
+    # extract nodes
+    used_nodes = set()
+
+    def extract_nodes(sub_workflow):
+        for x in sub_workflow['nodes']:
+            node_name = x.get('type')
+
+            # skip virtual nodes
+            if node_name in ['Reroute', 'Note']:
+                continue
+
+            if node_name is not None and not node_name.startswith('workflow/'):
+                used_nodes.add(node_name)
+
+    if 'nodes' in workflow:
+        extract_nodes(workflow)
+
+        if 'extra' in workflow:
+            if 'groupNodes' in workflow['extra']:
+                for x in workflow['extra']['groupNodes'].values():
+                    extract_nodes(x)
+
+    # lookup dependent custom nodes
+    ext_map = ext_node_map
+
+    rext_map = {}
+    preemption_map = {}
+    patterns = []
+    for k, v in ext_map.items():
+        if k == 'https://github.com/comfyanonymous/ComfyUI':
+            for x in v[0]:
+                if x not in preemption_map:
+                    preemption_map[x] = []
+
+                preemption_map[x] = k
+            continue
+
+        for x in v[0]:
+            if x not in rext_map:
+                rext_map[x] = []
+
+            rext_map[x].append(k)
+
+        if 'preemptions' in v[1]:
+            for x in v[1]['preemptions']:
+                if x not in preemption_map:
+                    preemption_map[x] = []
+
+                preemption_map[x] = k
+
+        if 'nodename_pattern' in v[1]:
+            patterns.append((v[1]['nodename_pattern'], k))
+
+    # identify used extensions
+    used_exts = set()
+    unknown_nodes = set()
+
+    for node_name in used_nodes:
+        ext = preemption_map.get(node_name)
+
+        if ext is None:
+            ext = rext_map.get(node_name)
+            if ext is not None:
+                ext = ext[0]
+
+        if ext is None:
+            for pat_ext in patterns:
+                if re.search(pat_ext[0], node_name):
+                    ext = pat_ext[1]
+                    break
+
+        if ext == 'https://github.com/comfyanonymous/ComfyUI':
+            pass
+        elif ext is not None:
+            if 'Fooocus' in ext:
+                print(f">> {node_name}")
+
+            used_exts.add(ext)
+        else:
+            unknown_nodes.add(node_name)
+
+    return used_exts, unknown_nodes
