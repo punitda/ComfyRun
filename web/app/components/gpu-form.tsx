a374c6450b46f6fea28bcb37e0c8e57f9f516592
punitda <punitdama@gmail.com>
2024-07-26 18:56:03 +0530

    Add input validation to machine name field

--- web/app/components/gpu-form.tsx
+++ web/app/components/gpu-form.tsx
@@ -14,7 +14,11 @@ import {
 } from "~/components/ui/select";
 import { GPU, OutputCustomNodesJson, OutputModel } from "~/lib/types";
 import { useFetcher } from "@remix-run/react";
+import { useState } from "react";
 
+const machineNameRegex = new RegExp(/^[a-zA-Z0-9._-]{1,63}$/);
+const machineNameErrorMsg =
+  "Machine name may contain only alphanumeric characters, dashes, periods, and underscores, and must be shorter than 64 characters";
 export interface GpuFormProps {
   customNodesJson: OutputCustomNodesJson;
   modelsJson: OutputModel[];
@@ -28,6 +32,7 @@ export default function GpuForm({
 }: GpuFormProps) {
   const fetcher = useFetcher();
   const isCreatingMachine = fetcher.state !== "idle";
+  const [machineNameError, setMachineNameError] = useState<string | null>(null);
   return (
     <fetcher.Form action="/create-machine" method="post">
       <div className="px-4 py-6 sm:p-8">
@@ -40,9 +45,20 @@ export default function GpuForm({
                 id="machine-name"
                 name="machine_name"
                 placeholder="Machine name"
+                onChange={(e) => {
+                  if (machineNameRegex.test(e.target.value)) {
+                    setMachineNameError(null);
+                  } else {
+                    setMachineNameError(machineNameErrorMsg);
+                  }
+                }}
+                maxLength={64}
                 required
               />
             </div>
+            {machineNameError ? (
+              <p className="mt-2 text-xs text-red-600">{machineNameError}</p>
+            ) : null}
           </div>
           <div className="sm:col-span-4">
             <Label>Select GPU</Label>
