e319991941baf1b4e9e675a3f265addb1ff5ba9c
punitda <punitdama@gmail.com>
2024-07-31 19:56:13 +0530

    Create custom hook for showing invalid email error toast messages

---
+++ web/app/lib/hooks.ts
@@ -0,0 +1,25 @@
+import { useSearchParams } from "@remix-run/react";
+import { useEffect } from "react";
+import { EMAIL_NOT_ALLOWED } from "./types";
+import { toast } from "~/components/ui/use-toast";
+
+export type Page = "SIGN-IN" | "SIGN-UP";
+export function useEmailNotAllowedHook(page: Page) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const signInError = searchParams.get("error");
+
+    if (signInError === EMAIL_NOT_ALLOWED) {
+      toast({
+        variant: "destructive",
+        title: page === "SIGN-IN" ? "Sign In error" : "Sign Up error",
+        description: `You can only ${
+          page === "SIGN-IN" ? "sign-in" : "signup"
+        } using email addresses added to the whitelist in .env file`,
+      });
+      setSearchParams(new URLSearchParams());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchParams]);
+}
