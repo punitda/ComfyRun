72357787ecd650a7afb2e74d17a394c22b9bf7c9
punitda <punitdama@gmail.com>
2024-08-12 10:50:49 +0530

    Move open app link from Dropdown menu to app name

--- web/app/routes/apps/route.tsx
+++ web/app/routes/apps/route.tsx
@@ -252,8 +252,14 @@ function AppsLayout({ apps }: AppsLayoutProps) {
               return (
                 <tr key={app.app_id}>
                   <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                    <div className="truncate text-sm font-medium leading-6 text-primary/80">
-                      {app.description}
+                    <div className="truncate text-sm font-medium leading-6 text-primary/80 hover:underline">
+                      <Link
+                        to={app.url}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        {app.description}
+                      </Link>
                     </div>
                   </td>
 
@@ -329,11 +335,6 @@ function DropdownActionMenu({ app }: DropdownActionMenuProps) {
           </DialogTrigger>
           <DropdownMenuItem>
             <Link to={`/create-app?rebuild=${app.description}`}>Rebuild</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link to={app.url} rel="noopener noreferrer" target="_blank">
-              Open App
-            </Link>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
