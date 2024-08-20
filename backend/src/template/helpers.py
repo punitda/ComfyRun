d625a8f20e676cb3ce43010138a30b86cd9a1064
punitda <punitdama@gmail.com>
2024-08-20 22:06:14 +0530

    Update workflow

--- backend/src/template/helpers.py
+++ backend/src/template/helpers.py
@@ -4,22 +4,11 @@ import shutil
 import os
 from modal import (Volume)
 
-models_volume = Volume.from_name("model-checkpoints", create_if_missing=True)
+models_volume = Volume.from_name("comfyui-models", create_if_missing=True)
 
 MOUNT_PATH: Path = Path("/mnt")
 MODELS_PATH: Path = MOUNT_PATH / "models"
 
-
-CLIP_VISION_ROOT_PATH: Path = Path("/mnt/models/clip_vision")
-SOURCE_CLIP_VISION_BIG_PATH: Path = CLIP_VISION_ROOT_PATH / \
-    "ipadapter-clip-vision-big/model.safetensors"
-SOURCE_CLIP_VISION_SMALL_PATH: Path = CLIP_VISION_ROOT_PATH / \
-    "ipadapter-clip-vision-small/model.safetensors"
-
-DEST_CLIP_VISION_BIG_PATH: Path = CLIP_VISION_ROOT_PATH / \
-    "CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors"
-DEST_CLIP_VISION_SMALL_PATH: Path = CLIP_VISION_ROOT_PATH / \
-    "CLIP-ViT-H-14-laion2B-s32B-b79K.safetensors"
 
 ANTELOPEV2_MODEL_ZIP_PATH: Path = Path(
     "/mnt/models/insightface/models/antelopev2.zip")
@@ -35,21 +24,24 @@ def download_models(models, civitai_token) -> bool:
         model_name = model["name"]
         download_url = model["url"]
         download_path = model["path"]
-        file_name = download_url.split("/")[-1]
-        if CIVITAI_BASE_URL in download_url or file_name in common_model_names:
+        file_name = model.get("filename", download_url.split("/")[-1])
+        print(f"file_name: {file_name}")
+        if file_name in common_model_names:
             checkpoint_path: Path = Path(
                 f"/mnt/models/{download_path}") / model_name
             relative_path: Path = checkpoint_path
         else:
             checkpoint_path: Path = Path(
                 f"/mnt/models/{download_path}") / file_name
-            relative_path = Path(download_path)
+            relative_path = Path(f"/mnt/models/{download_path}")
         print(f"checkpoint_path: {checkpoint_path}")
         print(f"checkpoint_path exists : {checkpoint_path.exists()}")
 
+        print(f"relative_path: {relative_path}")
+
         if not checkpoint_path.exists():
             print(f"Downloading {model_name} ....")
-            cmd = f"comfy --skip-prompt model download --url {download_url} --relative-path {relative_path} --set-civitai-api-token {civitai_token}"
+            cmd = f"comfy --skip-prompt model download --url {download_url} --relative-path {relative_path} --filename {file_name} --set-civitai-api-token {civitai_token}"
             download_process = subprocess.Popen(
                 cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             # Wait for the process to complete
@@ -65,16 +57,6 @@ def download_models(models, civitai_token) -> bool:
         else:
             print(f"skipping download of {model_name}. File exists")
     return True
-
-
-def move_clip_vision_files():
-    if SOURCE_CLIP_VISION_BIG_PATH.exists() and not DEST_CLIP_VISION_BIG_PATH.exists():
-        print(f"Moving big clip vision model to {DEST_CLIP_VISION_BIG_PATH}")
-        shutil.move(SOURCE_CLIP_VISION_BIG_PATH, DEST_CLIP_VISION_BIG_PATH)
-    if SOURCE_CLIP_VISION_SMALL_PATH.exists() and not DEST_CLIP_VISION_SMALL_PATH.exists():
-        print(
-            f"Moving small clip vision model to {DEST_CLIP_VISION_SMALL_PATH}")
-        shutil.move(SOURCE_CLIP_VISION_SMALL_PATH, DEST_CLIP_VISION_SMALL_PATH)
 
 
 def unzip_insight_face_models():
