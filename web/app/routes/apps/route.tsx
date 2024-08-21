bfb891ae4a95631b75a7c79aa6394b3dd088c8c2
punitda <punitdama@gmail.com>
2024-08-21 17:36:13 +0530

    Link to /app/{appName}/edit route

--- web/app/routes/apps/route.tsx
+++ web/app/routes/apps/route.tsx
@@ -254,9 +254,7 @@ function AppsLayout({ apps }: AppsLayoutProps) {
                   <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
                     <div className="truncate text-sm font-medium leading-6 text-primary/80 underline">
                       <Link
-                        to={app.url}
-                        rel="noopener noreferrer"
-                        target="_blank"
+                        to={`/app/${app.description}/edit`}
                       >
                         {app.description}
                       </Link>
