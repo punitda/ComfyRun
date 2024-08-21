d225e4d298094531e4b8aeb7549fc1b7d7d52b1a
punitda <punitdama@gmail.com>
2024-08-21 17:35:13 +0530

    Add EditingWorkflow for accessing ComfyUI via tunnels

--- backend/src/template/workflow.py
+++ backend/src/template/workflow.py
@@ -4,7 +4,8 @@ import json
 import os
 from config import config
 
-from modal import (App, Image, web_server, build, Secret)
+from modal import (App, Image, web_server, build,
+                   Secret, method, forward, web_endpoint, Queue)
 from helpers import (models_volume, MODELS_PATH, MOUNT_PATH,
                      download_models,  unzip_insight_face_models)
 
@@ -21,7 +22,6 @@ comfyui_image = (Image.debian_slim(python_version="3.10")
                  .apt_install("git")
                  .pip_install(dependencies)
                  .run_commands("comfy --skip-prompt install --nvidia")
-                 .run_commands("comfy --version")
                  .copy_local_file(f"{current_directory}/custom_nodes.json", "/root/")
                  .run_commands("comfy --skip-prompt node install-deps --deps=/root/custom_nodes.json")
                  .copy_local_file(f"{current_directory}/models.json", "/root/")
@@ -74,3 +74,53 @@ class ComfyWorkflow:
     @web_server(8188, startup_timeout=60)
     def ui(self):
         self._run_comfyui_server()
+
+
+@app.cls(
+    cpu=4.0,
+    memory=16384,
+    image=comfyui_image,
+    timeout=300,
+)
+class EditingWorkflow:
+    @build()
+    def download(self):
+        with open("/root/models.json", 'r', encoding='utf-8') as file:
+            models = json.load(file)
+            downloaded = download_models(models, os.environ["CIVITAI_TOKEN"])
+            models_volume.commit()
+            if downloaded:
+                print(
+                    "Copying models to correct directory - This might take a few more seconds")
+                shutil.copytree(
+                    MODELS_PATH, "/root/comfy/ComfyUI/models", dirs_exist_ok=True)
+                print("Models copied!!")
+                unzip_insight_face_models()
+
+    @method()
+    def run_comfy_in_tunnel(self, q):
+        with forward(8888) as tunnel:
+            url = tunnel.url
+            print(f"Starting ComfyUI at {url}")
+            q.put(url)
+            subprocess.run(
+                [
+                    "comfy",
+                    "--skip-prompt",
+                    "launch",
+                    "--",
+                    "--cpu",
+                    "--listen",
+                    "0.0.0.0",
+                    "--port",
+                    "8888",
+                ],
+                check=False
+            )
+
+    @web_endpoint(method="GET")
+    def get_tunnel_url(self):
+        with Queue.ephemeral() as q:
+            self.run_comfy_in_tunnel.spawn(q)
+            url = q.get()
+            return {"edit_url": url}
